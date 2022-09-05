44ef826e887253d6284a852979abea289ecfa0e3
silver <silver36c@gmail.com>
2022-09-06 04:31:20 +0900

    FEAT: 회원가입 유저 id 저장 로직 추가 (아직 회원가입 안 됨)

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -13,9 +13,10 @@ function SignUp () {
     const navigate = useNavigate();
 
 
+    
     const submit = () =>{
 
-        alert(email);
+
         let data = {
             email: email,
             password: password,
@@ -23,32 +24,40 @@ function SignUp () {
             userNickname: userNickName,
         }
 
+
+
+        // alert(data);
         let url = `${BASE_URL.auth}/auth/signup`;
+        console.log("axios")
         axios.post(url,  JSON.stringify(data), {
             headers: {
                 "Content-Type": `application/json`,
             },
             })
             .then((res) => {
-                navigate('/survey', {replace: true});
-                console.log(res);
+                alert("signup success");
+
+                console.log(res.data);
+                sessionStorage.setItem("signupUserId", res.data.userId);
                 alert(res)
+                navigate('/survey', {replace: true});
         }).catch(error => {
             alert(error)
             console.log(error.response)
         });
 
+        alert("axios 끝");
             
     }
-
+    
     return (<div className="SignUp">
         <p>SignUp</p>
-        <form>
+        <form onSubmit={submit}>
             <p>EMAIL: <input className="SignUp_ID" type="text" onChange={(e)=>{setEmail(e.target.value)}}/> </p>
             <p>PW: <input className="SignUp_Password" type="password" onChange={(e)=>{setPasword(e.target.value)}}/> </p>
             <p>userName: <input className="SignUp_Name" type="text" onChange={(e)=>{setUserName(e.target.value)}}/> </p>
             <p>userNickName: <input className="SignUp_NickName" type="text" onChange={(e)=>{setUserNickName(e.target.value)}}/> </p>
-            <MyButton type = {'default'} text ={'SignUp'} onClick={() => submit()}>
+            <MyButton type = {'submit'} text ={'SignUp'} >
           
         </MyButton>
         </form>
